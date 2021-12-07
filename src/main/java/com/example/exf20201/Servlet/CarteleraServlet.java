43b29bc0134e4482b14d594ed7fcb123d85c7444
Ronny Pastor <ronnypastorkolmakov@gmail.com>
2021-12-07 12:36:01 -0500

    Cartelera Cine Cadena y Pelicula Dao

--- src/main/java/com/example/exf20201/Servlet/CarteleraServlet.java
+++ src/main/java/com/example/exf20201/Servlet/CarteleraServlet.java
@@ -1,9 +1,13 @@
 package com.example.exf20201.Servlet;
 
+import com.example.exf20201.Dao.CineDao;
+import com.example.exf20201.Dao.PeliculaDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "CarteleraServlet", value = "/Cartelera")
 public class CarteleraServlet extends HttpServlet {
@@ -11,12 +15,63 @@ public class CarteleraServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        RequestDispatcher view = request.getRequestDispatcher("cartelera/lista.jsp");
-        view.forward(request, response);
+
+        String action = request.getParameter("action") != null ? request.getParameter("action") : "lista";
+        RequestDispatcher view;
+
+        if(action.equalsIgnoreCase("lista")){
+            //listas a enviar
+            view = request.getRequestDispatcher("cartelera/lista.jsp");
+            view.forward(request, response);
+
+        }else if(action.equalsIgnoreCase("agregar")){
+            PeliculaDao peliculaDao = new PeliculaDao();
+            CineDao cineDao = new CineDao();
+
+            request.setAttribute("listaPeliculas",peliculaDao.listaPeliculas());
+            request.setAttribute("listaCine",cineDao.listaCines());
+            view = request.getRequestDispatcher("cartelera/agregarFuncion.jsp");
+            view.forward(request, response);
+
+        }else if(action.equalsIgnoreCase("borrar")){
+
+
+
+
+        }else if(action.equalsIgnoreCase("editar")){
+
+
+
+        }
+
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if(action.equalsIgnoreCase("agregar")){
+            String idPeliculaStr = request.getParameter("idPelicula");
+            int idPelicula = Integer.parseInt(idPeliculaStr);
+
+            String idCineStr = request.getParameter("idCine");
+            int idCine = Integer.parseInt(idCineStr);
+
+            String formatoStr = request.getParameter("formato");
+            int formato = Integer.parseInt(formatoStr);
+
+            String lenguaje = request.getParameter("lenguaje");
+            String horario = request.getParameter("horario");
+
+
+
+
+
+        }
+
+
+
 
     }
 }
